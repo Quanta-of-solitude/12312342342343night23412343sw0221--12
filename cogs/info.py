81eeb2b9202b7be5f3b9582ca9f86b54f6e66d1d
Quanta-of-solitude <34452724+Quanta-of-solitude@users.noreply.github.com>
2018-03-16 19:34:58 +0530

    about

--- cogs/info.py
+++ cogs/info.py
@@ -6,17 +6,17 @@ class Trader:
         self.bot = bot
 
     @commands.command()
-    async def help(self,ctx):
+    async def about(self,ctx):
         '''help menu'''
-        await ctx.send("""```
-Help Menu:
-
-1.n!svrtime: To get the AE servertime.
-2.n!gif: To get gif based on a search (Not accurate)
-
-More commands and stuff will be added later.
-        ```""")
-
+        about = """```Made By Neophyte#5556\n\n\nNight Watch: Watching over the server 24/7 (Seriously, Not Stalking XD)\n\n"Hard Coded" to work only in Night Watch Server!```"""
+        helpm = "```\n1. n!svrtime: To get the AE servertime.\n2. n!gif [query]: Search gif.\n\nMore coming Later!\n\nPlus, Logging #gold-raven-sets 24/7 👍 and Stalking the member activities :3```"
+        em = discord.Embed(color = 0xffd500)
+        em.set_thumbnail(url = self.bot.user.avatar_url)
+        em.set_author(name = "About Night∆Watch:", icon_url = "https://image.ibb.co/bZ6yHx/profile.png")
+        em.add_field(name = "Info:", value = about, inline = False)
+        em.add_field(name = "Help:", value = helpm,inline = False)
+        em.set_footer(text = "|NightWatch|",icon_url = self.bot.user.avatar_url)
+        await ctx.send(embed = em)
 
 
 
