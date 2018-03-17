130d50e70e54f74f29bba6dfd969950807366556
Quanta-of-solitude <34452724+Quanta-of-solitude@users.noreply.github.com>
2018-03-17 21:26:59 +0530

    Add files via upload

--- cogs/utils.py
+++ cogs/utils.py
@@ -133,5 +133,136 @@ class Utility:
             return f'```py\n{e.__class__.__name__}: {e}\n```'
         return f'```py\n{e.text}{"^":>{e.offset}}\n{e.__class__.__name__}: {e}```'
 
+    def get_parts(self, string):
+        '''
+        Splits the sections of the embed command
+        '''
+        for i, char in enumerate(string):
+            if char == "{":
+                ret = ""
+                while char != "}":
+                    i += 1
+                    char = string[i]
+                    ret += char
+                yield ret.rstrip('}')
+
+    def parse_field(self, string):
+        '''
+        Recursive function to get all the key val
+        pairs in each section of the parsed embed command
+        '''
+        ret = {}
+
+        parts = string.split(':')
+        key = parts[0].strip().lower()
+        val = ':'.join(parts[1:]).strip()
+
+        ret[key] = val
+
+        if '|' in string:
+            string = string.split('|')
+            for part in string:
+                ret.update(self.parse_field(part))
+        return ret
+
+
+
+    async def to_embed(self, ctx, params):
+        '''Actually formats the parsed parameters into an Embed'''
+        em = discord.Embed()
+
+        if not params.count('{'):
+            if not params.count('}'):
+                em.description = params
+
+        for field in self.get_parts(params):
+            data = self.parse_field(field)
+
+            color = data.get('color') or data.get('colour')
+            if color == 'random':
+                em.color = random.randint(0, 0xFFFFFF)
+            elif color == 'chosen':
+                maybe_col = os.environ.get('COLOR')
+                if maybe_col:
+                    raw = int(maybe_col.strip('#'), 16)
+                    return discord.Color(value=raw)
+                else:
+                    return await ctx.send('Chosen color is not defined.')
+
+            elif color:
+                color = int(color.strip('#'), 16)
+                em.color = discord.Color(color)
+
+            if data.get('description'):
+                em.description = data['description']
+
+            if data.get('desc'):
+                em.description = data['desc']
+
+            if data.get('title'):
+                em.title = data['title']
+
+            if data.get('url'):
+                em.url = data['url']
+
+            author = data.get('author')
+            icon, url = data.get('icon'), data.get('url')
+
+            if author:
+                em._author = {'name': author}
+                if icon:
+                    em._author['icon_url'] = icon
+                if url:
+                    em._author['url'] = url
+
+            field, value = data.get('field'), data.get('value')
+            inline = False if str(data.get('inline')).lower() == 'false' else True
+            if field and value:
+                em.add_field(name=field, value=value, inline=inline)
+
+            if data.get('thumbnail'):
+                em._thumbnail = {'url': data['thumbnail']}
+
+            if data.get('image'):
+                em._image = {'url': data['image']}
+
+            if data.get('footer'):
+                em._footer = {'text': data.get('footer')}
+                if data.get('icon'):
+                    em._footer['icon_url'] = data.get('icon')
+
+            if 'timestamp' in data.keys() and len(data.keys()) == 1:
+                em.timestamp = ctx.message.created_at
+
+        return em
+
+
+
+
+    @commands.command()
+    async def embed(self, ctx, *, params):
+        '''Send complex rich embeds with this command!
+
+        ```
+        {description: Discord format supported}
+        {title: required | url: optional}
+        {author: required | icon: optional | url: optional}
+        {image: image_url_here}
+        {thumbnail: image_url_here}
+        {field: required | value: required}
+        {footer: footer_text_here | icon: optional}
+        {timestamp} <-this will include a timestamp
+        ```
+        '''
+        em = await self.to_embed(ctx, params)
+        #await ctx.message.delete()
+        try:
+            await ctx.send(embed=em)
+            self._last_embed = params
+        except:
+            await ctx.send('Improperly formatted embed!')
+
+
+
 def setup(bot):
     bot.add_cog(Utility(bot))
